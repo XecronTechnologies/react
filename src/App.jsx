e075629f03cbbef9f771a37c22d0e94a1b9b4eca
xecrontech <xecrontech@gmail.com>
2025-06-18 19:14:39 +0000

    Restaurant

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RestaurantWebsite from './RestaurantWebsite'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Hello Sathish</h1>
+   <RestaurantWebsite />
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
   // Optional: Set base path if deploying to subdirectory
   base: '/',
 })
